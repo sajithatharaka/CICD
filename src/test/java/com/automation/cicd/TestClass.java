69bc81dc25157caff7d3f1a3680c0f9264e11e52
Sajitha Tharaka <sajithatharaka@gmail.com>
2020-03-04 06:18:11 +0530

    updating to run in selenium hub

--- src/test/java/com/automation/cicd/TestClass.java
+++ src/test/java/com/automation/cicd/TestClass.java
@@ -1,9 +1,14 @@
 package com.automation.cicd;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,16 +18,21 @@ public class TestClass {
 	public static WebDriver driver;
 	
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
+		DesiredCapabilities dr=null;
+		dr=DesiredCapabilities.chrome();
+		dr.setBrowserName("chrome");
+		dr.setPlatform(Platform.WINDOWS);
+		
 		System.setProperty("webdriver.chrome.driver", driverLocation);
-		driver=new ChromeDriver();
+		driver=new RemoteWebDriver(new URL("http://192.168.56.1:4444/wd/hub"),dr);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
 	
 	@Test
 	public void Test1() {
-		driver.navigate().to("www.google.com");
+		driver.get("www.google.com");
 		System.out.print("Test1 title is "+ driver.getTitle());
 	}
 	
